d3a9a28be1ebe0f3b426cf3e8be8b874dc95f00b
Georgi Boshnakov <georgi.boshnakov@gmail.com>
2021-05-21 08:40:22 +0100

    add a check in testthat.R for the Windows check error

--- tests/testthat.R
+++ tests/testthat.R
@@ -4,3 +4,9 @@ if(require("testthat")) {
     test_check("rbibutils")
 } else
     warning("package 'testthat' required for 'rbibutils\'' tests")
+
+## temporary for the Windows check problem:
+bibdir <- system.file("bib", package = "rbibutils")
+fn  <- file.path(bibdir, "litprog280no_macros.bib")
+if(!file.exists(fn))
+    warning("file ", fn, "doesn't exist!")
