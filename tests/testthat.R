d89ba3134b63885cd85492fcb9bf08282149a835
Georgi Boshnakov <georgi.boshnakov@gmail.com>
2021-05-21 09:04:32 +0100

    the test code in previous commit was at wrong place

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,12 +1,14 @@
 if(require("testthat")) {
     library(testthat)
     library(rbibutils)
+
+    ## temporary for the Windows check problem:
+    bibdir <- system.file("bib", package = "rbibutils")
+    fn  <- file.path(bibdir, "litprog280no_macros.bib")
+    if(!file.exists(fn))
+        warning("file ", fn, "doesn't exist!")
+    
     test_check("rbibutils")
 } else
     warning("package 'testthat' required for 'rbibutils\'' tests")
 
-## temporary for the Windows check problem:
-bibdir <- system.file("bib", package = "rbibutils")
-fn  <- file.path(bibdir, "litprog280no_macros.bib")
-if(!file.exists(fn))
-    warning("file ", fn, "doesn't exist!")
